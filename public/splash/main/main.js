e3b00673b985283d105a88133c173988e0ab7642
chsong-alticast <bsk9212@gmail.com>
2023-07-10 17:51:23 +0900

    feat(img): bg, warp 등 이미지 추가, warp 동적 이미지 적용, setup json 추가

--- public/splash/main/main.js
+++ public/splash/main/main.js
@@ -1,7 +1,15 @@
-
-
 const W = 1000;
 const H = 500;
+const W_SCREEN = 1500;
+
+const IMG_PATH = "../../images/"
+
+let setupData;
+fetch("../config/setup/setup.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => setupData = jsondata);
 
 window.onload = function(){
 	const canvas = document.createElement("canvas");
@@ -38,13 +46,14 @@ window.onload = function(){
             case 13: 
                 if(type === 'wait'){
                     type = 'start'
+                    draw();
                     window.cancelAnimationFrame(draw)
                     setTimeout(function(){
                         console.log("start")
                         window.requestAnimationFrame(origin)
                     },1000);
                 }
-                break; //left
+                break;
         }
     })
 
@@ -59,7 +68,12 @@ window.onload = function(){
     function draw(){
         context.strokeStyle = "rgba(0,0,0,0.5)";
         context.drawImage(imgBg,bgx,0,1000,500,0,0,W*2,H*2)
-        context.drawImage(stopImgChar,x-w,y-(h*3),w*2,h*2);
+        context.drawImage(stopImgChar,x-w,y-(h*3),w*2,h*2);        
+        if(type === 'wait'){
+            const openText = new Image();
+            openText.src = "../../images/common/open.png";
+            context.drawImage(openText,550,550);
+        }
         // context.drawImage(imgBg,1125,250,500,250,0,0,W*2,H*2)
         // context.drawImage(stopImgChar,x-(w*2),y-(h*6),w*4,h*4);
         window.requestAnimationFrame(draw);
@@ -67,7 +81,6 @@ window.onload = function(){
     let count = 0;
     function origin(){
         count += 10;
-        console.log(count);
         if(count < 1000){
             context.setTransform(2 - (count / 1000), 0, 0, 2 - (count / 1000) ,-1000 + count, -1000 + count)
             window.requestAnimationFrame(origin)
@@ -90,6 +103,8 @@ function start(stopI, moveI, bgI){
     const stopImgChar = stopI;
     const moveImgChar = moveI;
     const imgBg = bgI;
+
+    const warpImg = [];
 
     let x=1000, y=1000; //정 가운데로 위치
     let w=128, h=128; //플레이어 이미지의 절반 사이즈
@@ -150,6 +165,25 @@ function start(stopI, moveI, bgI){
         }
         moveImgChar.src = toggle ? "../../images/common/moveImg1_"+arrow+".png" : "../../images/common/moveImg2_"+arrow+".png"
     }
+    function warpDraw() {
+        const warp = setupData?.map?.warp ?? [];
+        warp.forEach((e,i) => {
+            if(warpImg.length <= i){
+                warpImg.push(new Image());
+            }
+            warpImg[i].src = IMG_PATH + "common/" + e.name + ".png";
+            let movex = 0;
+            let xPosition = 0;
+            if(e.position[0] > 0){
+                movex = bgx >= 0 ? bgx : 0;
+                xPosition = e.position[2] * 2;
+            }else{
+                movex = bgx >= W_SCREEN ? 0 : W_SCREEN - bgx;
+                xPosition = (e.position[2] - W_SCREEN) * 2;                
+            }
+            context.drawImage(warpImg[i],(movex/e.position[0]) * warpImg[i].width,0, warpImg[i].width, warpImg[i].height,xPosition,e.position[3] * 2,e.position[0]* 2,e.position[1] * 2 )
+        })
+    }
     const draw = function() {
         let imgChar;
         if(type === "move"){
@@ -161,23 +195,22 @@ function start(stopI, moveI, bgI){
             stopImgChar.src = "../../images/common/stopImg_"+arrow+".png" 
             imgChar = stopImgChar;
         }
-        console.log(x, y, w, h)
         context.strokeStyle = "rgba(0,0,0,0.5)";
-        context.drawImage(imgBg,bgx,0,1000,500,0,0,W*2,H*2)
+        context.drawImage(imgBg,bgx,0,W,H,0,0,W*2,H*2)
         context.drawImage(imgChar,x-w,y-(h*3),w*2,h*2);
+        warpDraw();
         // context.drawImage(imgChar,x-w,y-h,w*2,h*2);
         //window.requestAnimationFrame(draw);
     }
     //window.requestAnimationFrame(draw);
 
     const move = function() {
-        console.log(x, bgx);
-        if(bgx >= 0 && bgx <= 1500){
+        if(bgx >= 0 && bgx <= W_SCREEN){
             bgx+=dx;
         }else{
             if(x-w >= 0 && x+w <= 2000){
                 x += dx * 2;
-               if(x == 1000 && (bgx <= 1500 || bgx >= 0)){
+               if(x == 1000 && (bgx <= W_SCREEN || bgx >= 0)){
                     bgx += dx;
                 }
             }else if(x-w < 0){
