191c12a6601588da5691b9f2a087dd31c1e2cfeb
chsong-alticast <bsk9212@gmail.com>
2023-07-13 18:31:26 +0900

    feat(main): page 정리, btn 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <>
       <div className="grid grid-row-3">
-        <div className="grid place-items-center">
+        <div className="grid place-items-center mt-10">
           <img
             className="block m-auto"
             src="/images/common/introduce.png"
@@ -18,10 +19,28 @@ export default function Home() {
           className="grid place-items-center h-[500px]"
           dangerouslySetInnerHTML={{ __html: splash_html }}
         ></div>
-        <div className="grid place-items-center">
-          <button className="w-52 h-14 mt-5 font-bold text-xl border-orange-700 border-none hover:border-dotted border-2 rounded-lg bg-orange-200 hover:bg-orange-400 text-center">
-            <img className="block m-auto" src="/images/common/skip.png"></img>
-          </button>
+        <div className="grid grid-cols-2 place-items-center">
+          <Link
+            href="https://songchanheum.github.io/portfolio/"
+            target="_blank"
+            className="place-self-end"
+          >
+            <button className="w-72 h-14 mt-5 mr-5 font-bold text-xl border-cyan-900 border-none hover:border-dotted border-8 rounded-lg bg-blue-400 hover:bg-cyan-700">
+              <img
+                className="block m-auto"
+                src="/images/common/version.png"
+              ></img>
+            </button>
+          </Link>
+
+          <Link
+            href="https://songchanheum.github.io/portfolio/"
+            className="place-self-start"
+          >
+            <button className="w-72 h-14 mt-5 ml-5 font-bold text-xl border-orange-700 border-none hover:border-dotted border-8 rounded-lg bg-orange-200 hover:bg-orange-400">
+              <img className="block m-auto" src="/images/common/skip.png"></img>
+            </button>
+          </Link>
         </div>
       </div>
     </>
