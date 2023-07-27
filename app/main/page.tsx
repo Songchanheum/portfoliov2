ec30464b72388dfa3d9e2d4c959dffc6537f49d4
chsong-alticast <bsk9212@gmail.com>
2023-07-27 15:37:58 +0900

    Update page.tsx

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import { GET } from "../api/user/route";
 import MainDiv from "@/components/MainDiv";
 
+async function getUser() {
+  const res = await fetch("http://localhost:5001/api/user");
+  return res.json();
+}
 async function Main() {
-  // const getData = await GET();
-  // console.log(getData);
+  const getData = await getUser();
+  console.log(getData);
   return (
     <div className="w-full h-screen grid place-items-center">
       <MainDiv />
