90aaf289d8f951e058414184781c173eef0bcf8c
chsong-alticast <bsk9212@gmail.com>
2023-07-31 17:46:51 +0900

    feat(career): timeline 추가, 컴포넌트 분리

---
+++ app/main/introduce/career/components/DevTimelineComponents.tsx
@@ -0,0 +1,44 @@
+"use client";
+import PageHeader from "@/app/main/components/PageHeader";
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import ExperienceCard from "./ExperienceCard";
+import { useScroll, useSpring, useTransform, motion } from "framer-motion";
+
+const DevTimelineComponents = () => {
+  const [width, setWidth] = useState<number>(0);
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref.current.scrollWidth, ref.current.offsetWidth);
+      setWidth(ref.current.scrollWidth - ref.current.offsetWidth);
+    }
+  }, []);
+  return (
+    <div className="w-full h-screen relative mx-auto items-center" ref={ref}>
+      <PageHeader title="Dev Timeline" />
+      <div className="w-fit overflow-hidden">
+        <motion.div
+          drag="x"
+          dragConstraints={{
+            right: 0,
+            left: -width - 200,
+          }}
+          className="flex space-x-5 p-10 snap-x snap-mandatory"
+        >
+          <ExperienceCard />
+          <ExperienceCard />
+          <ExperienceCard />
+          <ExperienceCard />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default DevTimelineComponents;
---
+++ app/main/introduce/components/SkillComponents.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState } from "react";
+import PageHeader from "@/app/main/components/PageHeader";
+import Skill from "./Skill";
+import { skillData } from "../constants";
+
+const SkillComponents = () => {
+  const [dataIndex, setDataIndex] = useState<number>(0);
+  return (
+    <div>
+      <PageHeader title="Skill" />
+      <div className="container mx-auto h-full flex flex-col items-center gap-x-6">
+        <div>
+          <div className="flex gap-x-12 mx-auto mb-8 justify-center">
+            {skillData.map((item, index) => {
+              return (
+                <div
+                  key={item.title + index}
+                  className={`${
+                    index === dataIndex &&
+                    "text-violet-600 after:bg-violet-600 after:w-full after:transitio-all after:duration-300"
+                  } font-extrabold ursor-pointer capitalize text-2xl relative after:w-8 after:h-[2px] after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setDataIndex(index)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div className="min-h-[800px] flex relative flex-col px-10 space-y-5 mx-auto items-start">
+            {skillData[dataIndex].info.map((item, index) => {
+              return (
+                <div key={item.title + index}>
+                  <div className="w-full">
+                    <p className="text-gray-600 font-d2 font-bold">
+                      {item.title}
+                    </p>
+                  </div>
+                  <div className="grid xl:grid-cols-6 grid-cols-4 gap-5">
+                    {item.src.map((skillItem, skillIndex) => {
+                      return (
+                        <Skill
+                          key="skillIndex"
+                          src={`https://skillicons.dev/icons?i=${skillItem}&theme=light`}
+                          percent={item.percent[skillIndex]}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SkillComponents;
