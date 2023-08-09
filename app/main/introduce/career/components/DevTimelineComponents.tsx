82b76d04fcba4a0fc1b5be30fd25b7b952a4c27b
chsong-alticast <bsk9212@gmail.com>
2023-08-09 17:03:13 +0900

    fix(swiper): swiper > project, experience 적용, favicon 변경

--- app/main/introduce/career/components/DevTimelineComponents.tsx
+++ app/main/introduce/career/components/DevTimelineComponents.tsx
@@ -6,6 +6,20 @@ import { motion } from "framer-motion";
 import { experience } from "../constants";
 import { fadeIn } from "@/common/utils/variants";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import {
+  FreeMode,
+  Pagination,
+  Navigation,
+  EffectCoverflow,
+} from "swiper/modules";
+
 const DevTimelineComponents = () => {
   const [width, setWidth] = useState<number>(0);
   const ref = useRef<HTMLDivElement>(null);
@@ -15,16 +29,44 @@ const DevTimelineComponents = () => {
     }
   }, []);
   return (
-    <div className="w-full h-fit relative mx-auto items-center" ref={ref}>
+    <div
+      className="container w-full h-fit relative mx-auto items-center"
+      ref={ref}
+    >
       <PageHeader title="Dev Timeline" />
       <motion.div
         variants={fadeIn("down", 0.2)}
         initial="hidden"
         animate="show"
         exit="hidden"
-        className="w-fit overflow-hidden"
       >
-        <motion.div
+        <Swiper
+          effect={"coverflow"}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          modules={[FreeMode, Pagination, Navigation, EffectCoverflow]}
+          pagination={{ clickable: true }}
+          navigation
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 500,
+            modifier: 1,
+          }}
+          className="relative mx-auto w-full"
+        >
+          {experience?.map((item, index) => {
+            return (
+              <SwiperSlide
+                key={index}
+                className="relative !flex justify-center items-center !w-[300px] xs:!w-[400px] sm:!w-[500px] md:!w-[600px] xl:!w-[800px] bg-center bg-cover mb-10"
+              >
+                <ExperienceCard data={item} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        {/* <motion.div
           drag="x"
           dragConstraints={{
             right: 0,
@@ -35,7 +77,7 @@ const DevTimelineComponents = () => {
           {experience?.map((item, index) => {
             return <ExperienceCard data={item} key={index} />;
           })}
-        </motion.div>
+        </motion.div> */}
       </motion.div>
     </div>
   );
--- app/main/projects/page.tsx
+++ app/main/projects/page.tsx
@@ -2,12 +2,68 @@
 import PageHeader from "@/app/main/components/PageHeader";
 import PageWrapper from "@/common/PageWrapper";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import { Pagination, FreeMode } from "swiper/modules";
+
+import { RxArrowTopRight, RxDesktop } from "react-icons/rx";
+import { WORK } from "./constants";
+
+const WorkSlider = () => {
+  return (
+    <Swiper
+      breakpoints={{
+        320: { slidesPerView: 1, spaceBetween: 15 },
+        640: { slidesPerView: 3, spaceBetween: 15 },
+      }}
+      freeMode={true}
+      pagination={{ clickable: true }}
+      modules={[Pagination, FreeMode]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {WORK.map((e, i) => {
+        return (
+          <SwiperSlide key={e.title + i}>
+            <div className="bg-white font-d2 dark:bg-slate-800 opacity-90 h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:opacity-100 transition-all duration-300">
+              <div className="text-4xl text-red-600 mb-8">
+                {e.icon === "dev" ? <RxDesktop /> : <></>}
+              </div>
+              <div>
+                <div className="mb-2 text-lg">{e.title}</div>
+                <p className="leading-normal text-gray-800 dark:text-gray-200 text-base">
+                  {e.description}
+                </p>
+              </div>
+              <div className="text-3xl absolute top-3 right-3">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-red-600 transition-all duration-300" />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
 const page = () => {
   return (
     <PageWrapper>
       <div>
         <PageHeader title="Projects" />
+        <div className="container mx-auto">
+          <div className="flex flex-col xl:flex-row gap-x-8">
+            <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+              <h2 className="text-2xl xl:mt-8 font-jua">Business Work</h2>
+              <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">work</p>
+            </div>
+            <div className="w-full xl:max-w-[65%]">
+              <WorkSlider />
+            </div>
+          </div>
+        </div>
       </div>
     </PageWrapper>
   );
