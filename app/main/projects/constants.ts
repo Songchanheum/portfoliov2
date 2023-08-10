3c3db86c5738025ce0e4e219041485022e754ffc
chsong-alticast <bsk9212@gmail.com>
2023-08-10 15:41:19 +0900

    feat(project): 프로젝트 페이지 추가 완료, swiper style 일부 수정

--- app/main/projects/constants.ts
+++ app/main/projects/constants.ts
@@ -1,7 +1,90 @@
 export const WORK = [
   {
     title: "RAiDEA Service CS-ADMIN 개발",
-    description: "Admin Page F/E 개발",
+    description: "Admin Page F/E 개발 및 유지보수",
     icon: "dev",
   },
+  {
+    title: "BFF 개발 및 유지보수",
+    description: "MSA대응을 위한 BFF 유지보수",
+    icon: "system",
+  },
+  {
+    title: "사물인터넷 기반 상수관망 수질 및 수량 저전력 계측 시스템 개발",
+    description: "물, 수도 관련 SI 프로젝트 개발",
+    icon: "dev",
+  },
+  {
+    title: "수돗물 수질 이상여부 진단 키트 개발",
+    description: "물, 수도 관련 SI 프로젝트 개발",
+    icon: "dev",
+  },
+  {
+    title:
+      "비상운전(수계전환 등) 시 안전한 수돗물 공급을 위한 관로 운영지원시스템 구축",
+    description: "물, 수도 관련 SI 프로젝트 개발",
+    icon: "dev",
+  },
+  {
+    title: "아쿠아유나이티드 4.0 시스템 개발",
+    description: "물, 수도 관련 SI 프로젝트 개발",
+    icon: "dev",
+  },
+  {
+    title: "물정보앱(Smart Water Grid) - 내부과제",
+    description: "웹앱 개발 및 신입사원 교육자료 개발",
+    icon: "setting",
+  },
+  {
+    title: "KT Coupon Service 고도화",
+    description: "서비스 연동방식 변경으로 인한 Proxy API Gateway 구축",
+    icon: "system",
+  },
+  {
+    title: "마이크로템플릿 WEBAPP Service 고도화",
+    description: "WEBAPP 고도화",
+    icon: "html",
+  },
+  {
+    title: "Golfzon Matrix view",
+    description: "서비스 개발 (스트리머 서버)",
+    icon: "system",
+  },
+  {
+    title: "OTT Telebee 통합관리 시스템 구축 및 개발",
+    description: "CMS/BMS/API 개발",
+    icon: "dev",
+  },
+  {
+    title: "인터렉티브 타임슬라이스 시스템 개발",
+    description: "시스템 개발",
+    icon: "setting",
+  },
 ];
+
+export const PROJECT = {
+  slide: [
+    {
+      images: [
+        { src: "", url: "", title: "포트폴리오 V2" },
+        { src: "", url: "", title: "개발 블로그 V2" },
+        { src: "", url: "", title: "RESUME" },
+        { src: "", url: "", title: "포트폴리오 V1" },
+      ],
+    },
+    {
+      images: [
+        { src: "", url: "", title: "아파트 홍보 페이지" },
+        { src: "", url: "", title: "개발 블로그 V1" },
+        { src: "", url: "", title: "코인 오토트레이딩 프로그램" },
+        { src: "", url: "", title: "Vue Chat Page" },
+      ],
+    },
+    {
+      images: [
+        { src: "", url: "", title: "OIPF STB WEB Page" },
+        { src: "", url: "", title: "출섹체크 프로그램" },
+      ],
+    },
+  ],
+};
--- app/main/projects/page.tsx
+++ app/main/projects/page.tsx
@@ -1,51 +1,178 @@
 "use client";
 import PageHeader from "@/app/main/components/PageHeader";
 import PageWrapper from "@/common/PageWrapper";
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
-import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
-import { Pagination, FreeMode } from "swiper/modules";
+import { Pagination, Autoplay } from "swiper/modules";
 
-import { RxArrowTopRight, RxDesktop } from "react-icons/rx";
-import { WORK } from "./constants";
+import {
+  RxArrowTopRight,
+  RxDesktop,
+  RxLayers,
+  RxGlobe,
+  RxGear,
+} from "react-icons/rx";
+
+import { BsArrowRight } from "react-icons/bs";
+import { PROJECT, WORK } from "./constants";
+import Image from "next/image";
 
 const WorkSlider = () => {
+  const progressCircle = useRef<SVGSVGElement>(null);
+  const progressContent = useRef<HTMLDivElement>(null);
+  const onAutoplayTimeLeft = (_s: any, time: number, progress: number) => {
+    if (progressCircle.current && progressContent.current) {
+      progressCircle.current.style.setProperty(
+        "--progress",
+        String(1 - progress)
+      );
+      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  };
+  function Icon({ icon }: { icon: string }) {
+    if (icon === "dev") return <RxDesktop />;
+    else if (icon === "system") return <RxLayers />;
+    else if (icon === "html") return <RxGlobe />;
+    else if (icon === "setting") return <RxGear />;
+    else null;
+  }
   return (
-    <Swiper
-      breakpoints={{
-        320: { slidesPerView: 1, spaceBetween: 15 },
-        640: { slidesPerView: 3, spaceBetween: 15 },
-      }}
-      freeMode={true}
-      pagination={{ clickable: true }}
-      modules={[Pagination, FreeMode]}
-      className="h-[240px] sm:h-[340px]"
-    >
-      {WORK.map((e, i) => {
-        return (
-          <SwiperSlide key={e.title + i}>
-            <div className="bg-white font-d2 dark:bg-slate-800 opacity-90 h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:opacity-100 transition-all duration-300">
-              <div className="text-4xl text-red-600 mb-8">
-                {e.icon === "dev" ? <RxDesktop /> : <></>}
+    <div className="h-fit">
+      <Swiper
+        breakpoints={{
+          320: { slidesPerView: 1, spaceBetween: 15 },
+          720: { slidesPerView: 3, spaceBetween: 15 },
+        }}
+        pagination={{ clickable: true }}
+        modules={[Pagination, Autoplay]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        className="h-[240px] sm:h-[340px]"
+      >
+        {WORK.map((e, i) => {
+          return (
+            <SwiperSlide key={e.title + i}>
+              <div className="bg-white font-d2 dark:bg-slate-800 opacity-90 h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:opacity-100 transition-all duration-300">
+                <div className="text-4xl text-red-600 mb-8">
+                  <Icon icon={e.icon} />
+                </div>
+                <div>
+                  <div className="mb-2 text-lg">{e.title}</div>
+                  <p className="leading-normal text-gray-800 dark:text-gray-200 text-base">
+                    {e.description}
+                  </p>
+                </div>
+                <div className="text-3xl absolute top-3 right-3">
+                  <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-red-600 transition-all duration-300" />
+                </div>
               </div>
-              <div>
-                <div className="mb-2 text-lg">{e.title}</div>
-                <p className="leading-normal text-gray-800 dark:text-gray-200 text-base">
-                  {e.description}
-                </p>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div className="autoplay-progress !relative right-5 bottom-0 w-12 h-12 !flex float-right !items-center !justify-center !font-bold">
+        <svg
+          viewBox="0 0 48 48"
+          ref={progressCircle}
+          className="absolute top-0 left-0 w-full h-full stroke-[4px] dark:stroke-violet-400 stroke-orange-300 fill-none transform -rotate-90"
+        >
+          <circle
+            cx="24"
+            cy="24"
+            r="20"
+            fill="none"
+            strokeDasharray={125.6}
+            strokeDashoffset={`calc(125.6 * (1 - var(--progress)))`}
+          ></circle>
+        </svg>
+        <span ref={progressContent}></span>
+      </div>
+    </div>
+  );
+};
+const ProjectSlider = () => {
+  const progressCircle = useRef<SVGSVGElement>(null);
+  const progressContent = useRef<HTMLDivElement>(null);
+  const onAutoplayTimeLeft = (_s: any, time: number, progress: number) => {
+    if (progressCircle.current && progressContent.current) {
+      progressCircle.current.style.setProperty(
+        "--progress",
+        String(1 - progress)
+      );
+      progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+    }
+  };
+  return (
+    <div className="h-fit">
+      <Swiper
+        spaceBetween={10}
+        pagination={{ clickable: true }}
+        modules={[Pagination, Autoplay]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
+        className="h-[350px] sm:h-[420px] md:h-[500px] lg:h-[700px] xl:h-[550px]"
+      >
+        {PROJECT.slide.map((e, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+                {e.images.map((image, index) => {
+                  return (
+                    <div
+                      key={image.title + index}
+                      className="relative rounded-lg overflow-hidden flex items-center justify-center group"
+                    >
+                      <div className="flex items-center justify-center relative overflow-hidden">
+                        <Image
+                          src={image.src}
+                          width={500}
+                          height={300}
+                          alt=""
+                        />
+                        <div className="absolute inset-0 bg-gradient-to-l from-transparent via-pink-400 to-violet-600 opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                        <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 transition-all duration-300">
+                          <div className="flex items-center justify-center gap-x-2">
+                            <div>{image.title}</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-              <div className="text-3xl absolute top-3 right-3">
-                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-red-600 transition-all duration-300" />
-              </div>
-            </div>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <div className="autoplay-progress !relative right-5 bottom-0 z-[99] w-12 h-12 !flex float-right !items-center !justify-center !font-bold">
+        <svg
+          viewBox="0 0 48 48"
+          ref={progressCircle}
+          className="absolute top-0 left-0 z-[99] w-full h-full stroke-[4px] dark:stroke-violet-400 stroke-orange-300 fill-none transform -rotate-90"
+        >
+          <circle
+            cx="24"
+            cy="24"
+            r="20"
+            fill="none"
+            strokeDasharray={125.6}
+            strokeDashoffset={`calc(125.6 * (1 - var(--progress)))`}
+          ></circle>
+        </svg>
+        <span ref={progressContent}></span>
+      </div>
+    </div>
   );
 };
 const page = () => {
@@ -57,10 +184,27 @@ const page = () => {
           <div className="flex flex-col xl:flex-row gap-x-8">
             <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
               <h2 className="text-2xl xl:mt-8 font-jua">Business Work</h2>
-              <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">work</p>
+              <p className="mb-4 max-w-[400px] mx-auto lg:mx-0 font-do font-extralight text-gray-700 dark:text-gray-300">
+                경력 사항중 진행한 프로젝트로 F/E B/E 개발, 시스템 구축, 디자인,
+                UI/UX 서비스 진행 내역입니다.
+              </p>
             </div>
             <div className="w-full xl:max-w-[65%]">
               <WorkSlider />
+            </div>
+          </div>
+        </div>
+        <div className="container mx-auto mt-16">
+          <div className="flex flex-col xl:flex-row gap-x-8">
+            <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+              <h2 className="text-2xl xl:mt-8 font-jua">Toy Project</h2>
+              <p className="mb-4 max-w-[400px] mx-auto lg:mx-0 font-do font-extralight text-gray-700 dark:text-gray-300">
+                개인적으로 진행한 토이프로젝트 내역입니다. 개인 학습, 역량
+                강화를 목적으로 진행한 프로젝트입니다.
+              </p>
+            </div>
+            <div className="w-full xl:max-w-[65%]">
+              <ProjectSlider />
             </div>
           </div>
         </div>
