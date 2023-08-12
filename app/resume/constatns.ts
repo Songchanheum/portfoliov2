add59932b84a22126544f3bf9c7a345817910635
Song ChanHeum <bsk9212@gmail.com>
2023-08-13 00:16:14 +0900

    feat(resume): experiecne 추가

---
+++ app/resume/constatns.ts
@@ -0,0 +1,134 @@
+export const EXPERIENCE_RESUME = [
+  {
+    title: "(주)알티모빌리티 / 프론트엔드 개발자",
+    team: "플랫폼개발팀 - 연구원",
+    due: "2021.12.01 ~ 재직중 (9개월)",
+    img: "/images/career/altimobility.png",
+    skill: [
+      "React",
+      "javascript",
+      "Typescript",
+      "Node.js",
+      "Ant Design",
+      "styledcomponents",
+      "sass",
+    ],
+    project: [
+      {
+        title: "Admin / BizPortal 향 Boiler Plate 개발 (연구과제)",
+        due: "2023-02 ~ 2023-08",
+        url: "",
+      },
+      {
+        title: "RAiDEA 서비스 관리자용 Admin Page 개발 및 유지보수",
+        due: "2021-12 ~ 2023-08",
+        url: "",
+      },
+      {
+        title: "MSA 대응을 위한 BFF Service 개발 및 유지보수",
+        due: "2021-12 ~ 2023-08",
+        url: "",
+      },
+    ],
+  },
+  {
+    title: "(주)에코플래그 / 프론트엔드 개발자",
+    team: "ITO사업본부 - 대리",
+    due: "2020.11.23 ~ 2021.11.30 (1년)",
+    img: "/images/career/ecoflag.png",
+    skill: [
+      "React",
+      "javascript",
+      "Java",
+      "Spring",
+      "Postgresql",
+      "Jenkins",
+      "Linux",
+      "Apache Tomcat",
+      "sass",
+    ],
+    project: [
+      {
+        title: "사물인터넷 기반 상수관망 수질 및 수량 저전력 계측 시스템 개발",
+        due: "2021-07 ~ 2021-08",
+        url: "",
+      },
+      {
+        title:
+          "수돗물 수질 이상여부 진단 키트 개발 및 비상운전(수계전환 등) 시 안전한 수돗물 공급을 위한 관로 운영지원시스템 구축",
+        due: "2021-04 ~ 2021-07",
+        url: "",
+      },
+      {
+        title: "아쿠아유나이티드 4.0 시스템 개발",
+        due: "2021-04 ~ 2021-06",
+        url: "",
+      },
+      {
+        title: "물정보앱(Smart Water Grid) - 내부과제",
+        due: "2021-03 ~ 2021-06",
+        url: "",
+      },
+      {
+        title: "배출영향분석 표준 프로그램 유지관리(2021)",
+        due: "2020-12 ~ 2021-08",
+        url: "",
+      },
+      {
+        title: "전국오염원조사 DB 및 웹 시스템 구축(4)",
+        due: "2020-11 ~ 2021-03",
+        url: "",
+      },
+    ],
+  },
+  {
+    title: "(주)카피앤패이스트 / 웹 개발자",
+    team: "미디어플랫폼팀 - 대리",
+    due: "2016.11.07 ~ 2021.11.19 (4년)",
+    img: "/images/career/cnp.png",
+    skill: [
+      "Java",
+      "Spring",
+      "Postgresql",
+      "Mysql",
+      "Redis",
+      "Jenkins",
+      "Linux",
+      "Apache Tomcat",
+      "javascript",
+      "Angular",
+    ],
+    project: [
+      {
+        title: "KT Coupon Service 고도화",
+        due: "2020-07 ~ 2020-09",
+        url: "",
+      },
+      {
+        title: "마이크로템플릿 WEBAPP Service 고도화",
+        due: "2020-06 ~ 2020-09",
+        url: "",
+      },
+      {
+        title: "Golfzon Matrix view",
+        due: "2019-06 ~ 2019-11",
+        url: "",
+      },
+      {
+        title: "Skylife PPV Service 개발",
+        due: "2018-02 ~ 2018-09",
+        url: "",
+      },
+      {
+        title: "OTT Telebee 통합관리 시스템 구축 및 개발",
+        due: "2017-03 ~ 2017-09",
+        url: "",
+      },
+      {
+        title: "인터렉티브 타임슬라이스 시스템 개발",
+        due: "2016-12 ~ 2019-03",
+        url: "",
+      },
+    ],
+  },
+];
