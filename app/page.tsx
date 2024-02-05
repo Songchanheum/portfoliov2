16190d7d33c6cb277f9715d74fe1e70c5410df4b
chsong-alticast <bsk9212@gmail.com>
2024-02-05 17:46:17 +0900

    fix(error): useSearchParam Suspense fallback 추가, 오류수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { MdOutlineBookmarkBorder } from "react-icons/md";
-import { MdOutlineLibraryBooks } from "react-icons/md";
-import { MdOutlineGridOn } from "react-icons/md";
 import DevPostComponents from "./components/home/DevPostComponents";
 import HomeHeader from "./components/home/HomeHeader";
 import HomeSidebar from "./components/home/HomeSidebar";
 import InfoHeader from "./components/home/InfoHeader";
 import HomeTabList from "./components/home/HomeTabList";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -17,7 +15,9 @@ export default function Home() {
           <section className="mx-auto max-w-[975px] h-screen bg-white md:p-4 justify-start flex-col">
             <InfoHeader />
             <section className="h-screen border-t-[1px] border-none md:border-t-gray-300 w-full flex-col item-center justify-start">
-              <HomeTabList />
+              <Suspense fallback={<>Loading...</>}>
+                <HomeTabList />
+              </Suspense>
               <article className="grid grid-cols-3 w-full gap-1">
                 <DevPostComponents />
               </article>
