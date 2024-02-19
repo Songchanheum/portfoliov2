ae56bb00c68d87223a49a86052cbe32af03d9e66
chsong-alticast <bsk9212@gmail.com>
2024-02-19 14:01:20 +0900

    fix(main): 게시글 숫자 추가, route 정리

--- app/api/post/blog/post.ts
+++ app/api/post/blog/post.ts
@@ -7,7 +7,43 @@ import {
   Block,
   CollectionPropertySchemaMap,
 } from "notion-types";
-import { TPost } from "./route";
+
+export type TPostStatus = "Private" | "Public" | "PublicOnDetail";
+export type TPostType = "Post" | "Paper" | "Page";
+
+export type TPost = {
+  id: string;
+  date: { start_date: string };
+  type: TPostType[];
+  slug: string;
+  tags?: string[];
+  category?: string[];
+  summary?: string;
+  author?: {
+    id: string;
+    name: string;
+    profile_photo?: string;
+  }[];
+  title: string;
+  status: TPostStatus[];
+  createdTime: string;
+  fullWidth: boolean;
+  thumbnail?: string;
+};
+
+type FilterPostsOptions = {
+  acceptStatus?: TPostStatus[];
+  acceptType?: TPostType[];
+};
+
+const initialOption: FilterPostsOptions = {
+  acceptStatus: ["Public"],
+  acceptType: ["Post"],
+};
+const current = new Date();
+const tomorrow = new Date(current);
+tomorrow.setDate(tomorrow.getDate() + 1);
+tomorrow.setHours(0, 0, 0, 0);
 
 export default function getAllPageIds(
   response: ExtendedRecordMap,
@@ -220,3 +256,28 @@ export const getPosts = async () => {
     return posts;
   }
 };
+
+export function filterPosts(
+  posts: TPost[],
+  options: FilterPostsOptions = initialOption
+) {
+  const { acceptStatus = ["Public"], acceptType = ["Post", "Paper"] } = options;
+  const filteredPosts = posts
+    // filter data
+    .filter((post) => {
+      const postDate = new Date(post?.date?.start_date || post.createdTime);
+      if (!post.title || !post.slug || postDate > tomorrow) return false;
+      return true;
+    })
+    // filter status
+    .filter((post) => {
+      const postStatus = post.status[0];
+      return acceptStatus.includes(postStatus);
+    })
+    // filter type
+    .filter((post) => {
+      const postType = post.type[0];
+      return acceptType.includes(postType);
+    });
+  return filteredPosts;
+}
--- app/api/today/route.ts
+++ app/api/today/route.ts
@@ -1,6 +1,7 @@
 import { kv } from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 import { toStringByFormatting } from "@/common/utils/dateFormat";
+import { filterPosts, getPosts } from "../post/blog/post";
 
 export async function GET(_req: NextRequest) {
   const date = toStringByFormatting(new Date());
@@ -27,6 +28,9 @@ export async function POST(req: NextRequest) {
 async function getCount(date: string) {
   const today = await kv.llen(`today:${date}`);
   const total = await kv.get("total");
+  const post = await kv.llen(`post`);
+  const blog = await getPosts();
+  const project = await kv.llen(`project`);
 
-  return { today, total };
+  return { post: post + project + filterPosts(blog).length, today, total };
 }
